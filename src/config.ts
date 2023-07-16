5dbc64c5fd633abfe5aa76627ece14eee37ecfc0
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-07-16 12:18:34 +0000

    ♻️ Refactor based on the site structure

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,17 @@
 export const site = {
-  "name" : "cosmo-cube",
-  "claim": "optionated starter",
-  "desc" : "A modern Astro starter template, with CUBE CSS",
-  "description": "A modern Astro starter template, with CUBE CSS and a state of the art web development stack",
-  "title": "cosmo-cube | advanced astro theme",
+  name: "cosmo-cube",
+  claim: "optionated starter",
+  desc: "A modern Astro starter template, with CUBE CSS",
+  description:
+    "A modern Astro starter template, with CUBE CSS and a state of the art web development stack",
+  title: "cosmo-cube | advanced astro theme",
+};
+
+export const link = {
+  repo: "https://github.com/advanced-astro/cosmo-cube",
+  andybell: "https://andy-bell.co.uk",
+  astro: "https://astro.build",
+  build: "https://buildexcellentwebsit.es",
+  cube_css: "https://cube.fyi",
+  utopia: "https://utopia.fyi",
 };
