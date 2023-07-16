2f65cfa7ddeed61c5052584d8ab8649b096ce1d8
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-07-16 11:15:53 +0000

    🔍️ Fix meta data

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const site = {
-  name: "cosmo-cube",
-  claim: "optionated starter",
-  desc: "A modern Astro starter template, with CUBE CSS and a state of the art web development stack",
-  title: "cosmo-cube | advanced astro theme",
+  "name" : "cosmo-cube",
+  "claim": "optionated starter",
+  "desc" : "A modern Astro starter template, with CUBE CSS",
+  "description": "A modern Astro starter template, with CUBE CSS and a state of the art web development stack",
+  "title": "cosmo-cube | advanced astro theme",
 };
