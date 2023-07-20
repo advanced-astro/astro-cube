33cce699b4df95b1007e9a7b65390747e4333a8a
Rychardt Rothwërkz <redwerkz@gmail.com>
2023-07-20 20:18:47 +0000

    :zap: Add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
+import compressor from "astro-compressor";
 import sitemap from "@astrojs/sitemap";
+
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true,
+    assets: true
   },
   site: "https://cosmo-cube.vercel.app/",
-  integrations: [sitemap(), compress()],
+  integrations: [sitemap(), compress(), compressor()]
 });
